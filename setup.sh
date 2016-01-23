98892824778a367476480aed377f4d7392f63d8a
jackyliuxx <jackyliuxx@gmail.com>
2016-01-24 03:41:12 +0800

    fix bug

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-install_g++_5="YES"
+install_gcc_5="YES"
 install_clang_36="YES"
 git_username="jackyliuxx"
 git_email="jackyliuxx@gamil.com"
@@ -10,7 +10,7 @@ if [ $(id -u) != 0 ];then
     sudo="sudo"
 fi
 
-if [ install_g++_5 == "YES" ];then
+if [ $install_gcc_5 == "YES" ];then
     $sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 fi
 
@@ -19,7 +19,7 @@ $sudo apt-get update
 $sudo apt-get install -y wget curl build-essential cmake python-dev python3 python3-dev python3-pip mysql-client libmysqlclient-dev clang tmux
 
 #clang-3.6
-if [install_clang_36 == "YES" ];then
+if [ $install_clang_36 == "YES" ];then
     $sudo apt-get install -y clang-3.6
     $sudo rm -f /usr/bin/clang /usr/bin/clang++
     $sudo ln -s /usr/bin/clang-3.6 /usr/bin/clang
@@ -27,7 +27,7 @@ if [install_clang_36 == "YES" ];then
 fi
 
 #g++ 5
-if [ install_g++_5 == "YES" ];then
+if [ $install_gcc_5 == "YES" ];then
     $sudo apt-get install -y g++-5
     $sudo rm -f /usr/bin/gcc /usr/bin/g++
     $sudo ln -s /usr/bin/gcc-5 /usr/bin/gcc
@@ -41,10 +41,6 @@ git config --global user.email $git_email
 git config --global core.editor vim
 git config --global push.default simple
 
-#vim
-git clone https://github.com/jackyliuxx/.vim.git ~/.vim
-~/.vim/setup.sh
-
 #powerline fonts
 git clone https://github.com/powerline/fonts.git ./powerlinefonts
 ./powerlinefonts/install.sh
@@ -54,3 +50,7 @@ rm -rf powerlinefonts
 $sudo apt-get install -y zsh
 sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="rkj-repos"/g' ~/.zshrc
+
+#vim
+git clone https://github.com/jackyliuxx/.vim.git ~/.vim
+~/.vim/setup.sh
